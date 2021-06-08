5d4acb441db17a9605deb0bee023cd526632ac02
Tharun Pranav Sv <35001651+tp-bixxie@users.noreply.github.com>
2021-06-08 11:08:12 +0530

    Add files via upload

---
+++ Dictionary/GUI/dictionary_terminal.py
@@ -0,0 +1,26 @@
+import json
+import os
+from difflib import get_close_matches
+import easygui as eg
+
+x = open('D:\IP PROJECT\Dictionary\Terminal\dictdata.json',)
+data = json.load(x)
+
+def query(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    else:
+        return 'Please check the word, the word does not exist in the database.'
+
+title   = "Dictionary"
+text    = "Enter the word"
+d_text  = ""
+output  = eg.enterbox(text, title, d_text)
+
+
+result = query(str(output))
+if type(output) == list:
+    for y in output:
+        print(y)
+msg= eg.msgbox(result)
---
+++ Dictionary/Terminal/dictionary_terminal.py
@@ -0,0 +1,31 @@
+import json
+import os
+from difflib import get_close_matches
+
+
+x = open('D:\IP PROJECT\Dictionary\Terminal\dictdata.json',)
+data = json.load(x)
+
+def query(w):
+    w = w.lower()
+    if w in data:
+        print('THE MEANING FOR THE ENTERED WORD IS AS FOLLOW:\n') 
+        return data[w]
+    elif len(get_close_matches(w, data.keys()))>0:
+        yn = input("Did you mean %s instead? Enter Y if yes (or) N if no:" %get_close_matches(w,data.keys())[0])
+        if yn == 'Y':
+                return data[get_close_matches(w, data.keys())[0]]
+        elif yn == 'N':
+                return "The word does not exist in the database."
+        else:
+                return "We did not understand your entry"
+    else:
+        return 'The word does not exist in the database.'
+
+
+in_word = input("Enter the word:")
+output = query(in_word)
+if type(output) == list:
+    for y in output:
+        print(y)
+print(output)
